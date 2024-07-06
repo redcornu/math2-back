6c4b170664a346089ab5b068b29608f2523433ff
redcornu <58800516+redcornu@users.noreply.github.com>
2024-07-06 10:18:55 +0900

    프론트엔드 주소2 시작

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const corsOptions = {
   origin: [
     "https://port-0-test-back-lxlts66g89582f3b.sel5.cloudtype.app",
     "https://web-math-front-lxlts66g89582f3b.sel5.cloudtype.app",
+    "https://web-math-front-backup-lxlts66g89582f3b.sel5.cloudtype.app/",
     "http://localhost:3000",
   ], // 허용할 도메인 목록입니다.
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // 허용할 HTTP 메서드 목록입니다.
